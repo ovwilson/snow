bc2474b859137fba84a1716f29b18b98298eee6b
Orlando Wilson <orlandowilson@Orlandos-MBP.fios-router.home>
2020-03-26 17:12:28 -0400

    app  service for http connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
+import { Observable, of } from 'rxjs'
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  data$ : Observable<any> = of([]);
+
+  constructor(private service: AppService){}
+
+  private url = '/api/now/table/sc_request?sysparm_limit=1';
   title = 'snow';
+
+  ngOnInit(){
+    this.data$ = this.service.get(this.url).pipe(
+      catchError(err => [err])
+      );
+  }
+
+  
 }
